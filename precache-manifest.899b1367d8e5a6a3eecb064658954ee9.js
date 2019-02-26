c6324570a847e661b95d4c7cc9d6242d5ae186da
julianjohannesen <github email address>
2019-02-26 15:38:01 -0500

    Updates

---
+++ precache-manifest.899b1367d8e5a6a3eecb064658954ee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "93555254a3e246fe44ec9c4665914092",
+    "url": "/Find-Books/static/media/thumbnailDefault.93555254.png"
+  },
+  {
+    "revision": "c9a87a5dc4d05bfc4bfd",
+    "url": "/Find-Books/static/js/runtime~main.c9a87a5d.js"
+  },
+  {
+    "revision": "845cfc1fbdb0ea35261f",
+    "url": "/Find-Books/static/js/main.845cfc1f.chunk.js"
+  },
+  {
+    "revision": "d87560f7da5fc511d50c",
+    "url": "/Find-Books/static/js/2.d87560f7.chunk.js"
+  },
+  {
+    "revision": "845cfc1fbdb0ea35261f",
+    "url": "/Find-Books/static/css/main.29c28d01.chunk.css"
+  },
+  {
+    "revision": "ff1036d98faf69255cb4e2059c1ca6e9",
+    "url": "/Find-Books/index.html"
+  }
+];
